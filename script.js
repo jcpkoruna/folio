1439d8492543cbd1512f67cceb2a018827b6093d
jcpkoruna <jcpkoruna@gmail.com>
2023-09-15 13:34:10 -0800

    2

--- script.js
+++ script.js
@@ -8,24 +8,25 @@ const smMenuLink2 = document.querySelector('.main-header__sm-menu-link--2')
 const smMenuLink3 = document.querySelector('.main-header__sm-menu-link--3')
 const smMenuLink4 = document.querySelector('.main-header__sm-menu-link--4')
 
+
 smMenuBtn.addEventListener('click', () => {
   smMenu.style.transitionDelay = '0s'
   smMenu.classList.add('main-header__sm-menu--active')
 
   smMenuLink1.style.transitionDelay = '.5s'
-  smMenuLink1.style.transform = 'translateY(0)'
+  smMenuLink1.style.transform = 'translateY(50)'
   smMenuLink1.style.opacity = '1'
 
   smMenuLink2.style.transitionDelay = '.8s'
-  smMenuLink2.style.transform = 'translateY(0)'
+  smMenuLink2.style.transform = 'translateY(50)'
   smMenuLink2.style.opacity = '1'
 
   smMenuLink3.style.transitionDelay = '1.1s'
-  smMenuLink3.style.transform = 'translateY(0)'
+  smMenuLink3.style.transform = 'translateY(50)'
   smMenuLink3.style.opacity = '1'
 
   smMenuLink4.style.transitionDelay = '1.4s'
-  smMenuLink4.style.transform = 'translateY(0)'
+  smMenuLink4.style.transform = 'translateY(50)'
   smMenuLink4.style.opacity = '1'
 })
 
@@ -82,50 +83,50 @@ smMenuCloseBtn.addEventListener('click', () => {
 
 
 // ---
-const themeColorSelector = document.querySelector('.themeClrSelector')
-const themeColorSelectorInput = document.querySelector(
-  '.themeClrSelector__input'
-)
-const root = document.documentElement;
+// const themeColorSelector = document.querySelector('.themeClrSelector')
+// const themeColorSelectorInput = document.querySelector(
+//   '.themeClrSelector__input'
+// )
+// const root = document.documentElement;
 
 
 
-const hexToRgb = (hex) => {
-  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-  return result
-    ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
-      }
-    : null
-}
+// const hexToRgb = (hex) => {
+//   let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+//   return result
+//     ? {
+//         r: parseInt(result[1], 16),
+//         g: parseInt(result[2], 16),
+//         b: parseInt(result[3], 16),
+//       }
+//     : null
+// }
 
-const eventFire = (el, etype) => {
-  if (el.fireEvent) {
-    el.fireEvent('on' + etype)
-  } else {
-    let evObj = document.createEvent('Events')
-    evObj.initEvent(etype, true, false)
-    el.dispatchEvent(evObj)
-  }
-}
+// const eventFire = (el, etype) => {
+//   if (el.fireEvent) {
+//     el.fireEvent('on' + etype)
+//   } else {
+//     let evObj = document.createEvent('Events')
+//     evObj.initEvent(etype, true, false)
+//     el.dispatchEvent(evObj)
+//   }
+// }
 
-themeColorSelector.addEventListener('click', () => {
-  eventFire(themeColorSelectorInput, 'input')
-})
+// themeColorSelector.addEventListener('click', () => {
+//   eventFire(themeColorSelectorInput, 'input')
+// })
 
-const setDynamicColor = (color) => {
+// const setDynamicColor = (color) => {
 
-  const { r, g, b } = hexToRgb(`${color}`)
+//   const { r, g, b } = hexToRgb(`${color}`)
   
-  root.style.setProperty('--themeColor', `${r},${g},${b}`);
-  //localStorage.setItem('color', color)
-}
+//   root.style.setProperty('--themeColor', `${r},${g},${b}`);
+//   //localStorage.setItem('color', color)
+// }
 
-themeColorSelectorInput.addEventListener('input', (e) => {
-  setDynamicColor(e.target.value)
-})
+// themeColorSelectorInput.addEventListener('input', (e) => {
+//   setDynamicColor(e.target.value)
+// })
 
 // if (localStorage.getItem('color')) {
 //   let userSelectedColor = localStorage.getItem('color')
@@ -133,9 +134,9 @@ themeColorSelectorInput.addEventListener('input', (e) => {
 //   setDynamicColor(userSelectedColor)
 // }
 
-// ---
-const headerLogoConatiner = document.querySelector('.main-header__logo-container')
+// // ---
+// const headerLogoConatiner = document.querySelector('.main-header__logo-container')
 
-headerLogoConatiner.addEventListener('click', () => {
-  location.href = 'index.html'
-})
+// headerLogoConatiner.addEventListener('click', () => {
+//   location.href = 'index.html'
+// })
